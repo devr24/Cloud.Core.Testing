a8812e19084aab69a0019371867511df658857d0
Rob McCabe <robert.mccabe@outlook.com>
2020-04-23 10:54:16 +0100

    Update XunitTraitExtensions.cs

--- src/Cloud.Core.Testing/XunitTraitExtensions.cs
+++ src/Cloud.Core.Testing/XunitTraitExtensions.cs
@@ -7,7 +7,7 @@
     using Xunit.Sdk;
 
     /// <summary>
-    /// Decorates a test as a Unit Test, so that it runs in Continuous Integration builds.
+    /// Decorates a test as a Unit test.
     /// </summary>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
     public sealed class IsUnitAttribute : CloudCoreCategoryAttribute
@@ -19,7 +19,7 @@
     }
 
     /// <summary>
-    /// Decorates a test as an Integration Test, so that it runs in Continuous Integration builds.
+    /// Decorates a test as an Integration test.
     /// </summary>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
     public sealed class IsIntegrationAttribute : CloudCoreCategoryAttribute
@@ -31,7 +31,7 @@
     }
 
     /// <summary>
-    /// Decorates a test as a Read Only Integration Test, so that it runs in Continuous Integration builds and can also run in Production.
+    /// Decorates a test as a Read Only Integration test.
     /// </summary>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
     public sealed class IsIntegrationReadOnlyAttribute : CloudCoreCategoryAttribute
@@ -43,7 +43,7 @@
     }
 
     /// <summary>
-    /// Decorates a test as a Read Only Integration Test, so that it runs in Continuous Integration builds and can also run in Production.
+    /// Decorates a test as a Performance test.
     /// </summary>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
     public sealed class IsPerformanceAttribute : CloudCoreCategoryAttribute
@@ -55,8 +55,7 @@
     }
 
     /// <summary>
-    /// Decorates a test as a development entry point.
-    /// This test never runs on any automated build, is purely a development facility.
+    /// Decorates a test as a Development test.
     /// </summary>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
     public sealed class IsDevAttribute : CloudCoreCategoryAttribute
@@ -68,7 +67,7 @@
     }
 
     /// <summary>
-    /// Decorates a test as a smoke test entry point.
+    /// Decorates a test as a smoke test.
     /// </summary>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
     public sealed class IsSmokeAttribute : CloudCoreCategoryAttribute
@@ -80,8 +79,7 @@
     }
 
     /// <summary>
-    /// Base class for all the category model attributes. Contains a list of all the attribute categories
-    /// and supports multiple categories.
+    /// Base class for all the category attributes.
     /// </summary>
     [TraitDiscoverer(CloudCoreCategoryDiscoverer.FullyQualifiedName, CloudCoreCategoryDiscoverer.Namespace)]
     // ReSharper disable once InconsistentNaming
@@ -104,7 +102,7 @@
     }
 
     /// <summary>
-    /// Discoverer that provides Cloud core trait values to xUnit tests.
+    /// Discoverer Cloud core trait values.
     /// </summary>
     // ReSharper disable once InconsistentNaming
     public class CloudCoreCategoryDiscoverer : ITraitDiscoverer
